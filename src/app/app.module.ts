29a71a94c8e4320a5663af10b80763c1f24b80b3
javier_martin83 <javier_martin83@yahoo.es>
2019-08-24 12:40:39 +0200

    avances index

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { HeroComponent } from './hero/hero.component';
+import { PresentacionComponent } from './presentacion/presentacion.component';
+import { ClaimIndexComponent } from './claim-index/claim-index.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    HeroComponent,
+    PresentacionComponent,
+    ClaimIndexComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/claim-index/claim-index.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-claim-index',
+  templateUrl: './claim-index.component.html',
+  styleUrls: ['./claim-index.component.css']
+})
+export class ClaimIndexComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
